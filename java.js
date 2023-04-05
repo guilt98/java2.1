465e2491fcb3ef6660c0fff003e866b0db36fd0c
guilt98 <124409817+guilt98@users.noreply.github.com>
2023-04-05 07:24:22 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,29 @@
+var data1 = parseFloat(14.5);
+var data2 = parseInt(38);
+var data3 = parseInt(27);
+var data4 = parseFloat(28.9);
+var data5 = parseInt(200);
+
+var data11 = data1 - data3;
+var data12 = data3 - data5;
+var data13 = data2 - data4;
+var data14 = data3 - data4;
+var data15 = data5 - data1;
+
+var data21 = data1 + data5;
+var data22 = data2 + data4;
+var data23 = data3 + data2;
+var data24 = data4 + data3;
+var data25 = data2 + data1;
+
+var data31 = data2 * data4;
+var data32 = data3 * data5;
+var data33 = data4 * data1;
+var data34 = data5 * data2;
+var data35 = data1 * data3;
+
+var data41 = data3 / data5;
+var data42 = data4 / data1;
+var data43 = data5 / data2;
+var data44 = data1 / data3;
+var data45 = data2 / data4;
